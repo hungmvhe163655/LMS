a241e2d4c7b9e2c7de16ad3a5205753cde348ffb
Minh <112998452+minhtche161354@users.noreply.github.com>
2024-06-11 13:04:21 +0700

    Lemao

---
+++ LAS_Managing_System_Backend/LAS_BACKEND_MAIN/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repository;
+
+namespace LAS_BACKEND_MAIN.Controllers
+{
+    [Route("api/AUTHEN")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public LoginController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+    }
+}
