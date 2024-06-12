0b89ec7c45612f19bd07cd8d72452de5e35610ee
hungvietmai <hungmvhe163655@fpt.edu.vn>
2024-06-13 02:20:52 +0700

    Reorganize FE

--- LMS_FRONTEND/src/App.tsx
+++ LMS_FRONTEND/src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ReactDOM } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import "./App.css";
 import Login from "./components/pages/Authentication/Login/Login";
