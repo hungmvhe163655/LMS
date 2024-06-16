e04470255031c8bd7ab7f5907b715f32106a1d23
MaiVietHungHE16355 <hungmvhe163655@fpt.edu.vn>
2024-06-16 15:51:39 +0700

    Fix header

---
+++ LMS_FRONTEND/src/components/ui/header.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+import { Link } from './link';
+
+interface HeaderProps {
+  title: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+  return (
+    <header className='bg-gray-800 p-4 text-white'>
+      <div className='container mx-auto flex items-center justify-between'>
+        <h1 className='text-lg font-bold'>{title}</h1>
+        <ul className='flex space-x-4'>
+          <li>
+            <Link to='/' className='hover:text-gray-300'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to='/about' className='hover:text-gray-300'>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to='/contact' className='hover:text-gray-300'>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
