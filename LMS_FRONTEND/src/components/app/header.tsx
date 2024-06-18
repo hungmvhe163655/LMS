e88663c3b9e87c063e87f92de98d067d92da268b
MaiVietHungHE16355 <hungmvhe163655@fpt.edu.vn>
2024-06-18 17:08:34 +0700

    Fix Header

--- LMS_FRONTEND/src/components/app/header.tsx
+++ LMS_FRONTEND/src/components/app/header.tsx
@@ -1,36 +1,23 @@
-import * as React from 'react';
-
+import { Avatar, AvatarFallback } from '../ui/avatar';
 import { Link } from '../ui/link';
 
-interface HeaderProps {
-  title: string;
-}
-
-const Header: React.FC<HeaderProps> = ({ title }) => {
+function Header() {
   return (
-    <header className='bg-gray-800 p-4 text-white'>
-      <div className='container mx-auto flex items-center justify-between'>
-        <h1 className='text-lg font-bold'>{title}</h1>
-        <ul className='flex space-x-4'>
-          <li>
-            <Link to='/' className='hover:text-gray-300'>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to='/about' className='hover:text-gray-300'>
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to='/contact' className='hover:text-gray-300'>
-              Contact
-            </Link>
-          </li>
-        </ul>
+    <header className='flex items-center justify-between bg-gray-800 p-4'>
+      <div>
+        <Link to='/' className='text-2xl font-bold hover:text-gray-300'>
+          LMS
+        </Link>
       </div>
+      <ul>
+        <li>
+          <Avatar>
+            <AvatarFallback className='text-sm'>VH</AvatarFallback>
+          </Avatar>
+        </li>
+      </ul>
     </header>
   );
-};
+}
 
 export default Header;
