2959f74b00e1f1d29595352906baed624e452ebc
Minh <112998452+minhtche161354@users.noreply.github.com>
2024-06-20 14:54:35 +0700

    sdsdsd

--- LMS_BACKEND/Service/ServiceManager.cs
+++ LMS_BACKEND/Service/ServiceManager.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Amazon.S3;
+using AutoMapper;
 using Contracts.Interfaces;
 using Entities.ConfigurationModels;
 using Entities.Models;
@@ -22,6 +23,7 @@ namespace Service
         private readonly Lazy<IAccountService> _accountService;
         private readonly Lazy<IAuthenticationService> _authenticationService;
         private readonly Lazy<IMailService> _mailService;
+        private readonly Lazy<IFileService> _fileService;
         //
         public ServiceManager(
             IRepositoryManager repositoryManager,
@@ -30,15 +32,19 @@ namespace Service
             RoleManager<IdentityRole> roleManager,
             SmtpClient client,
             IConfiguration configuration,
-            IMemoryCache memoryCache) 
+            IMemoryCache memoryCache,
+            IAmazonS3 s3) 
         {
             _accountService = new Lazy<IAccountService>(() => new AccountService(repositoryManager, logger,mapper));
             _authenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(logger, mapper,userManager,configuration,roleManager));
             _mailService = new Lazy<IMailService>(() => new MailService(logger,client,userManager,memoryCache,repositoryManager));
+            _fileService = new Lazy<IFileService>(() => new FileService( s3, configuration));
         }
         public IAccountService AccountService => _accountService.Value;
         public IAuthenticationService AuthenticationService => _authenticationService.Value;
         public IMailService MailService => _mailService.Value;
+
+        public IFileService FileService => _fileService.Value;
 
 
     }
