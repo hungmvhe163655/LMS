0cec8bc53e5d9f7166c9ac25123bfde3c056d0c0
Minh <112998452+minhtche161354@users.noreply.github.com>
2024-07-01 15:13:50 +0700

    Update FolderClosureRepository.cs

--- LMS_BACKEND/Repository/FolderClosureRepository.cs
+++ LMS_BACKEND/Repository/FolderClosureRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts.Interfaces;
+using Entities.Exceptions;
 using Entities.Models;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,10 @@ namespace Repository
         {
             return FindAll(track).Where(x => x.DescendantID.Equals(Ancs_Id)).ToList();
         }
+        public IEnumerable<FolderClosure> FindDescendants(Guid guid, bool track)
+        {
+            return FindAll(track).Where(x => x.AncestorID.Equals(guid)).ToList();
+        }
         public async Task<bool> AddLeaf(IEnumerable<FolderClosure> hold)
         {
             foreach (var item in hold)
@@ -27,11 +32,16 @@ namespace Repository
         }
         public IEnumerable<FolderClosure> GetFolderContent(Guid Id, bool track)
         {
-            return FindAll(track).Where(x=>x.AncestorID.Equals(Id)&&x.Depth==1).ToList();
+            return FindAll(track).Where(x => x.AncestorID.Equals(Id) && x.Depth == 1).ToList();
         }
         public IEnumerable<FolderClosure> GetBranch(Guid Id, bool track)
         {
-            return FindAll(track).Where(x=>x.DescendantID.Equals(Id)).OrderByDescending(x=>x.Depth).ToList();
+            return FindAll(track).Where(x => x.DescendantID.Equals(Id)).OrderByDescending(x => x.Depth).ToList();
+        }
+        public void DeleteListFolder(IEnumerable<FolderClosure> folderClosures)
+        {
+            if (!folderClosures.Any()) throw new BadRequestException("Folder branch List can not be null");
+            DeleteRange(folderClosures);
         }
     }
 }
