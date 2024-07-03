8ea71f7246f4dd77677783327d0d16fde49fe779
Le Sy Thai <thailshe160614@fpt.edu.vn>
2024-07-03 22:58:17 +0700

    card

--- LMS_BACKEND/Repository/Extensions/RepositoryNewsExtensions.cs
+++ LMS_BACKEND/Repository/Extensions/RepositoryNewsExtensions.cs
@@ -22,5 +22,39 @@ namespace Repository.Extensions
 
             return news.Where(n => n.Title.ToLower().Contains(lowerCaseTerm));
         }
+
+        public static IQueryable<News> Sort(this IQueryable<News> news, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return news.OrderBy(n => n.CreatedDate);
+
+            var orderParams = orderByQueryString.Trim().Split(',');
+            var propertyInfor = typeof(News).GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = propertyInfor.FirstOrDefault(ni =>
+                    ni.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty == null)
+                    continue;
+
+                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
+            }
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+                return news.OrderBy(n => n.CreatedDate);
+
+            //return news.OrderBy(orderQuery);
+            return news;
+        }
     }
 }
