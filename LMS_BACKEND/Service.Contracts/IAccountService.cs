f2d95c7aa808e574a7192cd9b71c3b482d578316
Minh <112998452+minhtche161354@users.noreply.github.com>
2024-07-10 17:16:55 +0700

    lamao

--- LMS_BACKEND/Service.Contracts/IAccountService.cs
+++ LMS_BACKEND/Service.Contracts/IAccountService.cs
@@ -16,10 +16,10 @@ namespace Service.Contracts
         Task<IEnumerable<MinorAccountReturnModel>> GetAccountNameWithRole(string role);
         Task<IEnumerable<AccountReturnModel>> GetUserByRole(string role);
         Task<AccountReturnModel> GetUserByName(string userName);
-        Task<AccountReturnModel> GetUserByEmail(string email);
+        Task<AccountReturnModel> GetUserByEmail(string email, bool Verified);
         Task<AccountReturnModel> GetUserById(string id);
         Task<IEnumerable<AccountReturnModel>> GetVerifierAccounts(string userName);
-        Task<bool> UpdateAccountVerifyStatus(IEnumerable<string> userIdList,string verifier);
+        Task<bool> UpdateAccountVerifyStatus(IEnumerable<string> userIdList, string verifier);
         Task<bool> ChangePasswordAsync(string userId, string oldPassword, string newPassword);
         Task UpdateProfileAsync(string userId, UpdateProfileRequestModel model);
         Task<AccountDetailResponseModel> GetAccountDetail(string userId);
