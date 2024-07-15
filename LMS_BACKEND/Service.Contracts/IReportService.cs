5a2111091110bcd80dcc865eef65faf943190f4f
DESKTOP-5P81EVO\Minhm <minhtche161354@fpt.edu.vn>
2024-07-15 16:25:05 +0700

    sửa tí

--- LMS_BACKEND/Service.Contracts/IReportService.cs
+++ LMS_BACKEND/Service.Contracts/IReportService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Entities.Exceptions;
+using Entities.Models;
+using Shared.DataTransferObjects.RequestDTO;
+using Shared.DataTransferObjects.RequestParameters;
+using Shared.DataTransferObjects.ResponseDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +13,10 @@ namespace Service.Contracts
 {
     public interface IReportService
     {
-
+        Task<(IEnumerable<ReportResponseModel> data, MetaData meta)> GetallReportsWithParam(Guid deviceId, ReportRequestParameters param);
+        Task<ReportResponseModel> GetReport(Guid id);
+        Task CreateReport(CreateReportRequestModel model);
+        Task DeleteReport(Guid id);
+        Task UpdateReport(Guid id, UpdateReportRequestModel model);
     }
 }
--- LMS_BACKEND/Service/ServiceManager.cs
+++ LMS_BACKEND/Service/ServiceManager.cs
@@ -53,16 +53,27 @@ namespace Service
             )
         {
             _accountService = new Lazy<IAccountService>(() => new AccountService(repositoryManager, logger, mapper, userManager, roleManager));
+
             _authenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(logger, mapper, userManager, configuration, roleManager, repositoryManager));
+
             _mailService = new Lazy<IMailService>(() => new MailService(logger, client, userManager, memoryCache, repositoryManager));
+
             _newsService = new Lazy<INewsService>(() => new NewsService(logger, repositoryManager, mapper));
+
             _fileService = new Lazy<IFileService>(() => new FileService(clients3, configuration, mapper, repositoryManager));
+
             _notificationService = new Lazy<INotificationService>(() => new NotificationService(repositoryManager, notiHub));
+
             _folderService = new Lazy<IFolderService>(() => new FolderService());
+
             _scheduleService = new Lazy<IScheduleService>(() => new ScheduleService(repositoryManager, mapper));
+
             _taskService = new Lazy<ITaskService>(() => new TaskService(repositoryManager, mapper));
+
             _taskListService = new Lazy<ITaskListService>(() => new TaskListService(repositoryManager, mapper));
+
             _projectService = new Lazy<IProjectService>(() => new ProjectService(logger, repositoryManager, mapper));
+
             _reportService = new Lazy<IReportService>(() => new ReportService(repositoryManager, mapper));
         }
         public IAccountService AccountService => _accountService.Value;
@@ -74,9 +85,7 @@ namespace Service
         public IScheduleService ScheduleService => _scheduleService.Value;
         public ITaskService TaskService => _taskService.Value;
         public IReportService ReportService => _reportService.Value;
-
         public ITaskListService TaskListService => _taskListService.Value;
-
         public IProjectService ProjectService => _projectService.Value;
     }
 }
