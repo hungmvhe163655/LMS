88e64aafa48c221783b81efe8a5f19ce2005f194
Le Sy Thai <thailshe160614@fpt.edu.vn>
2024-07-17 08:23:04 +0700

    update

---
+++ LMS_BACKEND/Repository/Extensions/ProjectRepositoryExtension.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Models;
+using Repository.Extensions.Utility;
+using Shared.DataTransferObjects.RequestParameters;
+using System.Linq.Dynamic.Core;
+
+namespace Repository.Extensions
+{
+    public static class ProjectRepositoryExtension
+    {
+        public static IQueryable<Project> FilterProjects(this IQueryable<Project> projects, DateTime minCreatedDate, DateTime maxCreatedDate) => projects.Where(n => (DateTime.Compare(n.CreatedDate, minCreatedDate) > 0 && (DateTime.Compare(n.CreatedDate, maxCreatedDate) < 0)));
+
+        public static IQueryable<Project> Search(this IQueryable<Project> projects, ProjectRequestParameters parameters)
+        {
+            if (string.IsNullOrWhiteSpace(parameters.SearchTerm ?? "")) return projects;
+
+            var lowerCaseTerm = parameters.SearchTerm == null ? "" : parameters.SearchTerm.Trim().ToLower();
+
+            return projects.Where(n => n.Name.ToLower().Contains(lowerCaseTerm) || n.Description.ToLower().Contains(lowerCaseTerm));
+        }
+
+        public static IQueryable<Project> Sort(this IQueryable<Project> projects, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return projects.OrderBy(n => n.CreatedDate);
+
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<News>(orderByQueryString);
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+                return projects.OrderBy(n => n.CreatedDate);
+
+            return projects.OrderBy(orderQuery);
+        }
+    }
+}
