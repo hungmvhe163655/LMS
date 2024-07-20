e2d49525cefffdef8b64a07a96ea9edf7105503f
Minh <112998452+minhtche161354@users.noreply.github.com>
2024-07-20 20:43:32 +0700

    Update ITaskService.cs

--- LMS_BACKEND/Service.Contracts/ITaskService.cs
+++ LMS_BACKEND/Service.Contracts/ITaskService.cs
@@ -1,6 +1,7 @@
 ﻿using Entities.Models;
 using Shared.DataTransferObjects.RequestDTO;
 using Shared.DataTransferObjects.ResponseDTO;
+using Shared.DataTransferObjects.RequestParameters;
 
 namespace Service.Contracts
 {
@@ -16,6 +17,6 @@ namespace Service.Contracts
         Task<Guid> SaveChangesForPatch(TaskUpdateRequestModel taskToPatch, Tasks taskEntity, string userId);
         Task<bool> IsTaskListAvailable(Guid taskListId);
         Task<bool> IsMemberInProject(Guid taskListId, string userId);
-
+        Task<(IEnumerable<TaskResponseModel> tasks, MetaData metaData)> GetTasksByUser(string userId, TaskRequestParameters parameters);
     }
 }
