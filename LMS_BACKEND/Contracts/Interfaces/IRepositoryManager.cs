af6d5e4f57afc3eb4f35f63de69f5c7ffd0074dc
Minh <112998452+minhtche161354@users.noreply.github.com>
2024-07-20 20:22:00 +0700

    Update IRepositoryManager.cs

--- LMS_BACKEND/Contracts/Interfaces/IRepositoryManager.cs
+++ LMS_BACKEND/Contracts/Interfaces/IRepositoryManager.cs
@@ -1,22 +1,38 @@
-﻿namespace Contracts.Interfaces
+namespace Contracts.Interfaces
 {
     public interface IRepositoryManager
     {
         IReportRepository report { get; }
+
         IAccountRepository account { get; }
+
         INewsRepository news { get; }
+
         INotificationRepository notification { get; }
+
         IStudentDetailRepository studentDetail { get; }
+
         IFileRepository file { get; }
+
         IFolderRepository folder { get; }
+
         IFolderClosureRepository folderClosure { get; }
+
         IScheduleRepository schedule { get; }
+
         ITaskRepository task { get; }
+
         ITaskHistoryRepository taskHistory { get; }
+
         ITaskListRepository taskList { get; }
+
         IProjectRepository project { get; }
+
         IMemberRepository member { get; }
+
         IProjectTypeRepository projectType { get; }
+
+        IDeviceRepository device { get; }
         Task Save();
     }
 }
