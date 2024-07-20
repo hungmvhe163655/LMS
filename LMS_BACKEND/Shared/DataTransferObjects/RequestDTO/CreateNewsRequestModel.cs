359f228a8f312f51161341b637decfa252cec1f7
Minh <112998452+minhtche161354@users.noreply.github.com>
2024-07-20 20:45:37 +0700

    Update CreateNewsRequestModel.cs

--- LMS_BACKEND/Shared/DataTransferObjects/RequestDTO/CreateNewsRequestModel.cs
+++ LMS_BACKEND/Shared/DataTransferObjects/RequestDTO/CreateNewsRequestModel.cs
@@ -2,11 +2,12 @@
 {
     public class CreateNewsRequestModel
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid Id { get; set; }
         public string? CreatedBy { get; set; }
         public string? Content { get; set; }
-        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime CreatedDate { get; set; }
         public string Title { get; set; } = null!;
+        public List<string>? FileKey { get; set; }
 
     }
 }
