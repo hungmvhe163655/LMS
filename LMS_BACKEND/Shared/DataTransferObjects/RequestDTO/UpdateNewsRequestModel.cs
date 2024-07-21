de01b1a4f006b462f6b1cf6d390a8edd2dd838d9
Le Sy Thai <thailshe160614@fpt.edu.vn>
2024-07-22 00:18:51 +0700

    update

--- LMS_BACKEND/Shared/DataTransferObjects/RequestDTO/UpdateNewsRequestModel.cs
+++ LMS_BACKEND/Shared/DataTransferObjects/RequestDTO/UpdateNewsRequestModel.cs
@@ -5,6 +5,6 @@
         public Guid Id { get; set; }
         public string? Content { get; set; }
         public string Title { get; set; } = null!;
-
+        public List<string>? FileKey { get; set; }
     }
 }
